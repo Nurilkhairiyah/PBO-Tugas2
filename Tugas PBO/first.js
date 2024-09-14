e58f0b1dc8743cca65ed1cbff09d2d8a9123b624
Nurilkhairiyah <nurilkhairiyah1@gmail.com>
2024-09-14 23:19:35 +0700

    Add files via upload
    Nuril Khairiyah (2300231)
    SIK 3A

---
+++ Tugas PBO/first.js
@@ -0,0 +1,14 @@
+console.log("connected with external js file")
+
+var a = 1;
+var b = 2;
+if(a+b<4)
+{
+    console.log(a+b);
+}
+else if(a+b<1)
+{
+    console.log(a+b);
+}
+else
+console.log("hi");
